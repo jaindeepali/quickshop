a67256c636958219ae8e00f05b897eedbc04fc94
jaindeepali <djdeepalijain811@gmail.com>
2014-01-25 21:25:20 +0530

    Initial commit: Added limonade framework, config file and layouts

---
+++ php/config/config.example.php
@@ -0,0 +1,5 @@
+<?php
+global $ebay_app_id = '';
+global $foursquare_client_id = '';
+global $foursquare_client_secret = '';
+?>
---
+++ php/views/layout.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title><?=$title?></title>
+  <link rel="stylesheet" href="reset/style.css">
+  <link rel="stylesheet" href="normalize/style.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script type="text/javascript" src="js/jquery-1.9.1.min.jss"></script>
+</head>
+<body>
+<?=$content?>
+</body>
+</html>
